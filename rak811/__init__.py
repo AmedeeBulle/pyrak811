71b35c5e1a419d071ca5d8177d294c062a8a1e16
Philippe Vanhaesendonck <philippe.vanhaesendonck@e-bulles.be>
2022-01-31 14:20:32 +0100

    fix: :bug: fix package version
    Signed-off-by: Philippe Vanhaesendonck <philippe.vanhaesendonck@e-bulles.be>

--- rak811/__init__.py
+++ rak811/__init__.py
@@ -7,6 +7,6 @@ import pkg_resources
 from .exception import Rak811Error  # noqa: F401
 
 try:
-    __version__ = pkg_resources.get_distribution('setuptools').version
+    __version__ = pkg_resources.get_distribution("rak811").version
 except Exception:
-    __version__ = 'unknown'
+    __version__ = "unknown"
